902480b120076f749824ae980a527d9a32abad53
ysreedhar <ya.sree@gmail.com>
2018-03-16 18:25:24 +0800

    - Added Date & Time implememtation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main(){
   runApp(new MaterialApp(
@@ -16,6 +17,36 @@ class MyApp extends StatefulWidget{
 class _State extends State<MyApp> with WidgetsBindingObserver {
 
   String _lastPress ='never';
+  DateTime _date = new DateTime.now();
+  TimeOfDay _time = new TimeOfDay.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _date,
+        firstDate: new DateTime(2015),
+        lastDate: new DateTime(2020)
+    );
+    if(picked != null && picked != _date){
+      print('Date Selected: ${_date.toString()}');
+      setState((){
+        _date = picked;
+      });
+    }
+  }
+
+  Future<Null> _selectTime(BuildContext context) async{
+    final TimeOfDay picked = await showTimePicker(
+        context: context,
+        initialTime: _time
+    );
+    if(picked != null && picked != _time){
+      print('Time Selected: ${_time.toString()}');
+      setState((){
+        _time = picked;
+      });
+    }
+  }
 
   void _onPressed(){
     print('Pressed');
@@ -92,7 +123,15 @@ class _State extends State<MyApp> with WidgetsBindingObserver {
         padding: new EdgeInsets.all(32.0),
         child: new Column(
           children: <Widget>[
-            new Text('Last Pressed ${_lastPress}')
+            new Text('Last Pressed ${_lastPress}'),
+            new Text('Date Selected: ${_date.toString()}'),
+            new RaisedButton(
+              child: new Text('Select Date'),
+                onPressed: (){_selectDate(context);}),
+            new Text('Time Selected: ${_time.toString()}'),
+            new RaisedButton(
+                child: new Text('Select Time'),
+                onPressed: (){_selectTime(context);}),
           ],
         ),
       ),
